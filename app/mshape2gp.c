cf2839871dfa0aeb55a63d46caac5741348d5c0f
Tomomichi Sugihara <zhidao@ieee.org>
2019-08-09 16:44:07 +0900

    Ported mshape2gp from RoKi apps.

---
+++ app/mshape2gp.c
@@ -0,0 +1,40 @@
+#include <zeo/zeo.h>
+
+void usage(char *arg)
+{
+  eprintf( "Usage: %s <.ztk file>\n", arg );
+  exit( 0 );
+}
+
+void output_shape(zShape3D *s)
+{
+  register int i;
+
+  for( i=0; i<zShape3DFaceNum(s); i++ ){
+    zVec3DDataPrint( zShape3DFaceVert(s,i,0) );
+    zVec3DDataPrint( zShape3DFaceVert(s,i,1) );
+    printf( "\n" );
+    zVec3DDataPrint( zShape3DFaceVert(s,i,2) );
+    zVec3DDataPrint( zShape3DFaceVert(s,i,2) );
+    printf( "\n\n" );
+  }
+}
+
+void conv_mshape(char *filename)
+{
+  register int i;
+  zMShape3D ms;
+
+  if( !zMShape3DScanZTK( &ms, filename ) )
+    exit( EXIT_FAILURE );
+  for( i=0; i<zMShape3DShapeNum(&ms); i++ )
+    output_shape( zMShape3DShape(&ms,i) );
+  zMShape3DDestroy( &ms );
+}
+
+int main(int argc, char *argv[])
+{
+  if( argc < 2 ) usage( argv[0] );
+  conv_mshape( argv[1] );
+  return 0;
+}
